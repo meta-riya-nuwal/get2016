d8b92164a2cf74986b2dd64cfb7ea9ed00502fa0
meta-riya-nuwal <riya.nuwal@metacube.com>
2016-08-17 11:38:29 +0530

    Update ReadInput.java

--- ReadInput.java
+++ ReadInput.java
@@ -23,4 +23,26 @@ public class ReadInput {
 		}
 		return ans;
 	}
+	int getNumberOfUser() {
+
+		BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(System.in));
+
+		int users = 0;
+
+		try {
+
+			String user = bufferedReader.readLine();
+			users = Integer.parseInt(user);
+			if (users <= 0) {
+				System.out.print("Enter number of users greater than 0");
+				getNumberOfUser();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		return users;
+
+	}
 }
